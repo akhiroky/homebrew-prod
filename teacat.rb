df3fef903a62224e224ab45bab5dd8b20d362f8f
akhiroky <65586688+akhiroky@users.noreply.github.com>
2021-07-13 08:01:25 +0900

    Update teacat.rb

--- teacat.rb
+++ teacat.rb
@@ -3,7 +3,7 @@ VERSION = "1.0.0"
 class Teacat < Formula
   homepage "https://github.com/akhiroky/TeaCat"
   url "https://github.com/akhiroky/TeaCat/releases/download/v#{VERSION}-alpha/TeaCat-#{VERSION}_linux_amd64.tar.gz"
-  sha256 "6de411ff3e4b1658a413dd6181fcXXXXXXXXXXXXXXXXXXXX"
+  sha256 "a2de280f060ebe964964228c011fd9705b39886be9b428d3f7c3e8fdb579f251"
   version VERSION
 
   def install
