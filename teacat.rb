f13b14f2fdc75df4e6d5984dfbe9b695f08b0101
akhiroky <65586688+akhiroky@users.noreply.github.com>
2021-07-13 08:39:24 +0900

    Update teacat.rb

--- teacat.rb
+++ teacat.rb
@@ -1,10 +1,13 @@
 VERSION = "1.0.0"
 
 class Teacat < Formula
+  dec "Implements of wc and cat command."
   homepage "https://github.com/akhiroky/TeaCat"
   url "https://github.com/akhiroky/TeaCat/releases/download/v#{VERSION}-alpha/TeaCat-#{VERSION}_linux_amd64.tar.gz"
-  sha256 "a2de280f060ebe964964228c011fd9705b39886be9b428d3f7c3e8fdb579f251"
   version VERSION
+  sha256 "a2de280f060ebe964964228c011fd9705b39886be9b428d3f7c3e8fdb579f251"
+  license "CC0"
+  
 
   def install
     bin.install "teacat"
@@ -12,4 +15,5 @@ class Teacat < Formula
 
   test do
     system bin/"teacat", "-h"
+  end
 end
